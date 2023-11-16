1d64662da38275cc989159201c671af2c2bb4cbf
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-16 12:51:01 -0300

    feat: weakmap

--- Cap-5/main.js
+++ Cap-5/main.js
@@ -68,3 +68,48 @@ for (var value of map.values()){
 for(var entrada of map.entries()){
     console.log(entrada)
 }
+
+//weakmap
+//WeakMap é	uma	coleção	de	pares de	chave/valor	na	qual as	 chaves	 só	 podem ser objetos.
+//weakmap para armazenar dois elementos de uma pag HTML
+
+var	weakMap	=	new	WeakMap();
+var	elemento1	=	window;
+var	elemento2	=	document.querySelector('body'); //fazer exercicios
+weakMap.set(elemento1,	'sou	o	elemento1');
+weakMap.set(elemento2, 'sou o elemento 2');
+
+console.log(weakMap.get(elemento1));
+
+weakMap.delete(elemento2)
+console.log(weakMap.has('sou o elemento2'))
+
+elemento2.parentNode.removeChild(elemento2); //remove todas as referencias do elemento2, ou seja, o nó pai dele n doc
+elemento2 = null //remove referencia local
+
+//após isso, ao chamar com get o eelemento 2, a saida é undefined
+
+console.log(weakMap.get(elemento2));
+//com weakmap não é possível passar uma funcao, precisa ser um objeto
+// só é possível usar get,has,set e delete com weakmap, não é possível impar tudo usando clear ou usar entries
+//weakMap é pouco usado no dia a dia, na maior parte, usa o map
+//vantagem: podemos armazenar dados em um objeto e quando ele é excluido, o dado tbm é, liberando espaço e n havera memory leak
+var Pessoa = function(){
+	var dadosPrivados = new WeakMap()
+	function Pessoa(nome){
+		dadosPrivados.set(this,{nome:nome})  // Configura o dado privado para a instância atual
+	}
+	Pessoa.prototype.getNome = function(){
+		return dadosPrivados.get(this).nome //acessa a instancia atual
+    
+	}
+	return Pessoa
+}();
+
+var vitoria = new Pessoa('vitoria')
+console.log(vitoria.getNome())// Imprime o nome da instância 'vitoria' usando o método getNome()
+
+console.log(vitoria.nome) //undefined pq tenta acessar diretamente a propriedade "Nome" mas não permite
+
+
+
