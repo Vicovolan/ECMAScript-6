0ad194c6c68c709e35a0909de6b9a3d0a40cf3a7
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-16 08:46:58 -0300

    Feat: iterar

--- Cap-4/main.js
+++ Cap-4/main.js
@@ -5,3 +5,23 @@ var gatos = ['niko', 'milo'];
 var iteradorgatos = gatos[Symbol.iterator]();
 
 iteradorgatos.next();
+iteradorgatos.next();
+iteradorgatos.next();
+
+//Iterar cores, cada gato recebe uma cor aleatória
+
+
+function selecionar_cor(cor) {
+    console.log(cor);
+}
+
+var cores = ['amarelo', 'frajola', 'preto', 'branco', 'cinza', 'tricolor'];
+
+var iterador = gatos[Symbol.iterator]();
+var prox = iterador.next();
+
+while (!prox.done) {
+    var gato = prox.value;
+    selecionar_cor(cores[Math.floor(Math.random() * cores.length)]);
+    prox = iterador.next();
+}
