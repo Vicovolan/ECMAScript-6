506040e3be9bb26c57b4c35d4b2ea52579e340bd
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-16 10:31:16 -0300

    feat: for of

--- Cap-4/main.js
+++ Cap-4/main.js
@@ -1,11 +1,13 @@
 /* iterador */
 
-var gatos = ['niko', 'milo'];
+var gatos = ['niko', 'milo' ];
 
 var iteradorgatos = gatos[Symbol.iterator]();
 
 iteradorgatos.next();
+
 iteradorgatos.next();
+
 iteradorgatos.next();
 
 //Iterar cores, cada gato recebe uma cor aleatória
@@ -22,6 +24,25 @@ var prox = iterador.next();
 
 while (!prox.done) {
     var gato = prox.value;
-    selecionar_cor(cores[Math.floor(Math.random() * cores.length)]);
+    selecionar_cor(cores[Math.floor(Math.random() * cores.length)]); //math.floor garante que  o n é arredondado p baixo
     prox = iterador.next();
+}
+
+
+//for in, for of
+var	linkedin	=	{
+    nome:	'Vitoria',
+    idade : 20
+}
+
+for(var perfil in linkedin){
+
+    var info = linkedin[perfil]
+    console.log(info)
+}
+
+var dados = [3,4,5,9,10,11,24]
+
+for(numeros of dados){
+    console.log(numeros)
 }
