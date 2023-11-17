d7751f878a4e75563fee044f5391aac0325b000a
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-17 12:25:08 -0300

    feat:marcado

--- Cap-9/main.js
+++ Cap-9/main.js
@@ -11,8 +11,16 @@ console.log(`${n1}+${n2}`) // 2+3
 
 const	div	=
 `<div>
-		<p><b>Nome:</b>	${nome}</p>
-		<p><b>Idade:</b>	${idade}</p>
+	<p><b>Nome:</b>	${nome}</p>
+	<p><b>Idade:</b>	${idade}</p>
 
 </div>`
+
+//string marcada
+const horas = new Date().getHours()
+const mensagem = `Bom dia, são ${horas} horas`
+
+console.log(mensagem) //ainda é simples
+
+const	mensagem2	=	defineMensagem`Bom	dia, são ${horas} horas`; //marca a mensagem com a tag defineMensagem sem crase
 
