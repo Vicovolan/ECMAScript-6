9aedb04a7b8489087fbb64fc1350ac0265de5f88
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-17 11:41:32 -0300

    feat: string simple

---
+++ Cap-9/main.js
@@ -0,0 +1,18 @@
+//manipulação de textos com templates de string
+// string simples
+
+const	nome	=	'Julio';
+console.log(`Boa tarde,	${nome}`) //usando crase e n aspas simples
+
+//também é possível inserir expressão 
+const n1 = 2
+const n2 = 3
+console.log(`${n1}+${n2}`) // 2+3
+
+const	div	=
+`<div>
+		<p><b>Nome:</b>	${nome}</p>
+		<p><b>Idade:</b>	${idade}</p>
+
+</div>`
+
