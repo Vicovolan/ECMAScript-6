7c348d11f7cb006421fd53d90fa1c13426caf3f2
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-19 19:56:35 -0300

    feat: string marcada

--- Cap-9/main.js
+++ Cap-9/main.js
@@ -24,3 +24,27 @@ console.log(mensagem) //ainda é simples
 
 const	mensagem2	=	defineMensagem`Bom	dia, são ${horas} horas`; //marca a mensagem com a tag defineMensagem sem crase
 
+let listatempo = ['bom dia', 'boa tarde', 'boa noite']
+
+function lista(strings, ...values){
+    console.log(strings)
+    console.log(values)
+}
+
+
+function defineMensagem(strings, ...values) {
+    const hora = values[1];
+    if (hora >= 6 && hora <= 12) {
+        values[0] = "Bom dia";
+    } else if (hora > 12 && hora <= 18) {
+        values[0] = "Boa tarde";
+    } else {
+        values[0] = "Boa noite";
+    }
+    values[0] = `${values[0]}, são`;
+    return `${values[0]}${strings[0]}${hora}${strings[2]}`;
+}
+const	mensagem3 =	defineMensagem`${""}${horas}	horas`;
+
+console.log(mensagem3);
+
