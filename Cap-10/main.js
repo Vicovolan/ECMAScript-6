a9a27774be5644bcf71693b8dd2f689f9acec5ee
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-19 21:10:12 -0300

    feat: arrow functio

--- Cap-10/main.js
+++ Cap-10/main.js
@@ -10,4 +10,25 @@ function liga(){
 }
 //tbm existe construtor de função 
 var	desligar	=	new Function(arg1,arg2,'//	corpo	da	função')
+var parametro1, param2, param3
 //no ES6 tem ainda + uma maneira, arrow function.
+(parametro1,param2,param3) => { 
+    //corpo da função
+}
+//essa notação é menos verbosa e o contexto de execução é diferente
+var bemvindo = function(){
+
+}
+
+var bemvindo = ()=>{ //menos verbosa
+}
+//com só um parametro, é possível eliminar os parenteses, exemplo:
+
+var bemvindo = nome => {
+    console.log(`entre ${nome}`)
+}
+bemvindo("Vitoria")
+//arrow function tbm elimina o problema com o this, dele n achar a referencia
+//metodo bind, tbm usado para resolver o problema do this mas a arrow function faz c que n precisemos usar
+
+
