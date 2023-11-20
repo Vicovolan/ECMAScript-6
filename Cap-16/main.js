6577a8d60380a1c0f2c9e7144e978811d27ade6c
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-20 14:18:33 -0300

    feat: classe 167

--- Cap-16/main.js
+++ Cap-16/main.js
@@ -12,4 +12,49 @@ console.log(prototipo.modelo) //protótipo
 console.log(prototipo.chassi)//129938913
 console.log(prototipo.qtdPortas) //2
 
+function carro(modelo){
+    this.modelo = modelo
+}
 
+carro.prototype.andar = function(){
+    console.log("vrummm")
+}
+prototipo.andar()
+
+const volks = new	carro('Gol',	'9120219',	4);
+console.log(volks.modelo);	//	Gol
+volks.andar();	//	vrum	vrum
+
+class carro {
+    constructor (modelo,chassi,qtdportas){
+        this.modelo = modelo
+        this.chassi = chassi
+        this.qtdportas = qtdportas
+    }
+    andar(){
+        console.log("vrumm")
+    }
+}
+const	basico	=	new	carro('Básico',	'123123',	2);
+console.log(basico.modelo)//Básico
+
+class Sonix extends carro{
+    abrirTeto(){
+        console.log("abrindo teto solar")
+    }
+}
+const sonata = new Sonix;
+sonata.andar()
+//apenas com isso, ele nao recebe o modelo,chassi e qtdportas da classe pai. 
+class Sonix extends carro{
+    constructor(modelo,chassi,qtdportas){
+        super(modelo,chassi,qtdportas)
+    }
+
+    abrirTeto(){
+        console.log("abrindo teto solar")
+    }
+
+}
+//agr sim
+console.log(sonata.modelo) //Sonix
