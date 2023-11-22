9b029740abbec4a22222a2f943e09fcefcc95c7a
VitoriaCovolan <102617521+Vicovolan@users.noreply.github.com>
2023-11-22 11:32:41 -0300

    feat:ecma 2016

---
+++ Cap-22/main.js
@@ -0,0 +1,9 @@
+//operador exponecial 
+console.log(4**2)//16
+
+//saber se algo ja esta na lista
+const	curso	=	'Entendendo	o	ES6';
+curso.includes('ES6');	//	true
+
+let numeros = [1,2,9,3]
+numeros,includes(2) //true
